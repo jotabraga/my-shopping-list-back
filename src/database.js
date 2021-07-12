cba2e035bbd7038a75211da7fd766439eb80d4fe
jotabraga <jpmbraga.feg@gmail.com>
2021-07-12 16:46:49 -0300

    Tests done

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ import pg from "pg";
 const { Pool } = pg;
 
 const connection = new Pool({
-  user: "bootcamp_role",
+  user: "postgres",
   password: "123456",
   host: "localhost",
   port: 5432,
---
+++ tests/items.test.js
@@ -0,0 +1,25 @@
+import { expect } from "@jest/globals";
+import { beforeEach, it } from "jest-circus";
+import { async } from "regenerator-runtime";
+import supertest from "supertest";
+import { describe } from "yargs";
+import app from "../src/app.js";
+import connection from "../src/database.js"
+
+beforeEach(async () => {
+    await connection.query("DELETE FROM items");
+});
+
+describe("GET /items", () => {
+    it("returns status 200", async () => {
+        const result = await supertest(app).get("/items");
+        expect(result.status).toEqual(200);
+    });
+
+    it("returns the items in the database", async () =>{
+        await connection.query("INSERT INTO items (text) VALUES ('teste 1), ('teste 2')");
+
+        const result = await supertest(app).get("/items");
+        expect(result.body.length).toEqual(2);
+    });
+});
